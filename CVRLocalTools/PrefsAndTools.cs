043b872d3b5dcd0fd07363ff41d6ecd4e3bb8bbb
EtiTheSpirit <subzeroxan@gmail.com>
2022-11-23 06:48:11 -0600

    New bulk parameter assertions

--- CVRLocalTools/PrefsAndTools.cs
+++ CVRLocalTools/PrefsAndTools.cs
@@ -1,9 +1,11 @@
 ﻿using MelonLoader;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using UnityEngine;
 
 namespace CVRLocalTools {
@@ -20,11 +22,15 @@ namespace CVRLocalTools {
 		/// <summary>
 		/// If true, <see cref="AssertParameterIsLocal(Animator, string)"/> will raise a warning as intended. If false, it will do nothing.
 		/// </summary>
-		internal static bool WarnForFaults => _settingsCategory.GetEntry<bool>("WarnForNetworkedLocal").Value;
+		internal static bool WarnForFaults => _warnForFaultsEntry.Value;
+		internal static bool WarnForLocalAndGlobal => _warnForLocalAndGlobalEntry.Value;
+		private static MelonPreferences_Entry<bool> _warnForFaultsEntry;
+		private static MelonPreferences_Entry<bool> _warnForLocalAndGlobalEntry;
 
 		internal static void InitializePrefs() {
 			_settingsCategory = MelonPreferences.CreateCategory("Local Data Utilities");
-			_settingsCategory.CreateEntry("WarnForNetworkedLocal", false, "Warn if using networked parameters", "If true, raise a warning in the console if the avatar declares any parameter managed by this mod without a leading #. If disabled, the mod will just do nothing without saying why it's doing nothing.");
+			_warnForFaultsEntry = _settingsCategory.CreateEntry("WarnForNetworkedLocal", false, "Verify Parameter Names", "If true, raise a warning in the console if the avatar declares any parameter managed by this mod without a leading #. If disabled, the mod will just do nothing without saying why it's doing nothing.");
+			_warnForLocalAndGlobalEntry = _settingsCategory.CreateEntry("WarnForLocalAndGlobal", false, "Verbose Verification", $"If true, and if {_warnForFaultsEntry.DisplayName} is true, the system will also raise a notice in the console when it finds both \"Param\" *and* \"#Param\" on an avatar (avatar authors doing this is anticipated to be intentional, but if accidental it can be confusing).");
 			_settingsCategory.SaveToFile();
 		}
 
@@ -38,9 +44,10 @@ namespace CVRLocalTools {
 		/// <param name="localParamIfPresent">If present, this is a reference to the local parameter.</param>
 		/// <exception cref="ArgumentException">If <paramref name="name"/> includes a leading #.</exception>
 		/// <returns>True if everything is OK, false if a warning was raised.</returns>
+		[Obsolete("This is too expensive for repeated calls.", true)]
 		internal static bool AssertParameterIsLocal(this Animator animator, string name) {
 			if (animator == null) throw new ArgumentNullException(nameof(animator));
-			if (name == null) throw new ArgumentNullException(nameof(animator));
+			if (name == null) throw new ArgumentNullException(nameof(name));
 			if (name.StartsWith("#")) throw new ArgumentException($"The name input into {nameof(AssertParameterIsLocal)} starts with #. It should not start with #.", nameof(name));
 
 			string localName = "#" + name;
@@ -65,6 +72,54 @@ namespace CVRLocalTools {
 				return false;
 			}
 			return true;
+		}
+
+		/// <summary>
+		/// This method verifies that every name in the given array is associated with a local parameter in ChilloutVR. It has the additional duty
+		/// of warning the user if a global parameter was defined in its place, or if both a local and a global parameter were defined.
+		/// </summary>
+		/// <param name="animator"></param>
+		/// <param name="names"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		internal static bool AssertParametersAreLocal(Animator animator, string[] names) {
+			if (animator == null) throw new ArgumentNullException(nameof(animator));
+			if (names == null) throw new ArgumentNullException(nameof(names));
+			if (!WarnForFaults) {
+				return true;
+			}
+
+			bool ok = true;
+			IReadOnlyDictionary<string, AnimatorControllerParameter> nameLookup = animator.GetParameters();
+			for (int index = 0; index < names.Length; index++) {
+				string replicatedName = names[index];
+				if (replicatedName.Length > 0 && replicatedName[0] == '#') throw new ArgumentException($"A name input into {nameof(AssertParametersAreLocal)}, {replicatedName}, starts with #. It should not start with #.", nameof(names));
+
+				string localName = "#" + replicatedName;
+				bool hasGlobal = nameLookup.ContainsKey(replicatedName);
+				bool hasLocal = nameLookup.ContainsKey(localName);
+				if (hasGlobal && !hasLocal) {
+					LocalUtilsMain._log.Warning($"{YELLOW}The avatar you just loaded defines a parameter named {CYAN}{replicatedName}{YELLOW} (when it should instead be {UNDERLINE_ON}{GREEN}#{UNDERLINE_OFF}{CYAN}{replicatedName}{YELLOW}). Without the leading {GREEN}#{YELLOW}, the value would be sent across the network, which you don't want. This parameter will {UNDERLINE_ON}not{UNDERLINE_OFF} be changed by this mod. You can turn off this warning in your mod settings.");
+					ok = false;
+				} else if (hasGlobal && hasLocal) {
+					if (WarnForLocalAndGlobal) {
+						LocalUtilsMain._log.Warning($"{YELLOW}The avatar you just loaded defines both a parameter named {CYAN}{replicatedName}{YELLOW} {UNDERLINE_ON}and{UNDERLINE_OFF} a parameter named {GREEN}#{CYAN}{replicatedName}{YELLOW}). The mod will {UNDERLINE_ON}only{UNDERLINE_OFF} affect {GREEN}#{CYAN}{replicatedName}{YELLOW}, and {CYAN}{replicatedName}{YELLOW} will be ignored!");
+					}
+					ok = false;
+				}
+			}
+
+			return ok;
+		}
+
+		internal static IReadOnlyDictionary<string, AnimatorControllerParameter> GetParameters(this Animator animator) {
+			Dictionary<string, AnimatorControllerParameter> dict = new Dictionary<string, AnimatorControllerParameter>();
+			int count = animator.parameterCount;
+			for (int index = 0; index < count; index++) {
+				AnimatorControllerParameter param = animator.GetParameter(index);
+				dict[param.name] = param;
+			}
+			return dict;
 		}
 
 	}
