4fbf7bf0d4ec323ba7dd4115cdcbdbcb31e0fd21
EtiTheSpirit <subzeroxan@gmail.com>
2022-11-23 08:02:18 -0600

    Change name to "Local Parameter Extender"

--- CVRLocalTools/Properties/AssemblyInfo.cs
+++ CVRLocalTools/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CVRLocalTools")]
+[assembly: AssemblyTitle("LocalParameterExtender")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CVRLocalTools")]
+[assembly: AssemblyProduct("LocalParameterExtender")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
 [assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
-[assembly: MelonInfo(typeof(LocalUtilsMain), "CVRLocalUtils", "1.2.0", "Xan // Eti")]
+[assembly: MelonInfo(typeof(LocalUtilsMain), "Local Parameter Extender", "1.2.0", "Xan // Eti")]
